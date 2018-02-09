b56ba84f51a1e03e1836a805e5f2802800c1e944
Roadhog31 <RoadHog31@users.noreply.github.com>
2018-02-09 15:50:55 +0000

    Add project files.

---
+++ DecisionStatementsWolfAcademy/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DecisionStatementsWolfAcademy
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            /* Console.Write("Are you feeling happy or Sad? ");
+             string sAnswer = Console.ReadLine();
+             if (sAnswer == "Happy")
+             {
+                 Console.WriteLine("You are feeling Happy. "); 
+             }
+             else if (sAnswer == "Sad")
+             {
+                 Console.WriteLine("You are feeling Sad.");
+             }
+             else
+             {
+                 Console.WriteLine("Sorry, I only know Happy or Sad. "); 
+             }*/
+
+            Console.WriteLine("Do you enjoy C# programming? (Yes/No/Maybe)");
+            string sInput = Console.ReadLine();
+                switch (sInput)
+                {
+                    case "Yes":
+                        Console.WriteLine("That's Great!");
+                        break;
+                    case "Maybe":
+                        Console.WriteLine("Of course you do!");
+                        break; 
+                    case "No" :
+                        Console.WriteLine("Too bad!");
+                        break;
+                 }
+            
+        }
+    }
+}
